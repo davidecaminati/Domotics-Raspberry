2bf5c1779f263de0684a77d6205aaf5dcc6aff1f
Davide Caminati <davide.caminati@gmail.com>
2013-11-18 14:52:39 -0800

    translate to english

--- Hardware/Send_push_notification/send_notification.py
+++ Hardware/Send_push_notification/send_notification.py
@@ -1,21 +1,21 @@
 import httplib, urllib
 import subprocess
 
-lista_ip_dispositivi = ["211","202","111"]
+device_ip_list = ["211","202","111"]
 
-for ping in lista_ip_dispositivi:
+for ping in device_ip_list:
     address = "192.168.0." + str(ping)
     res = subprocess.call(['ping', '-c', '1', address])
     if res == 0:
         print "ping to", address, "OK "
     elif res == 2:
-        print "no response from", address
+        print "no response from ", address
         conn = httplib.HTTPSConnection("api.pushover.net:443")
 		conn.request("POST", "/1/messages.json",
 		urllib.urlencode({
-		"token": "abdf67yAvRcQufveo2nGkwKNi6xTHb",
-		"user": "u2v1vYFWvmGGNGN3Ffnn9NnCW1Y3xN",
-		"message": "hello world",
+		"token": "INSERT-TOKEN-HERE",
+		"user": "INSERT-USER-HERE",
+		"message": "no response from %s" % address,
 		}), { "Content-type": "application/x-www-form-urlencoded" })
 		conn.getresponse()
     else:
@@ -23,9 +23,9 @@ for ping in lista_ip_dispositivi:
         conn = httplib.HTTPSConnection("api.pushover.net:443")
 		conn.request("POST", "/1/messages.json",
 		urllib.urlencode({
-		"token": "abdf67yAvRcQufveo2nGkwKNi6xTHb",
-		"user": "u2v1vYFWvmGGNGN3Ffnn9NnCW1Y3xN",
-		"message": "hello world",
+		"token": "INSERT-TOKEN-HERE",
+		"user": "INSERT-USER-HERE",
+		"message": "no response from %s" % address,
 		}), { "Content-type": "application/x-www-form-urlencoded" })
 		conn.getresponse()
 

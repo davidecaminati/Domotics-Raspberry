11a4658b68bb9de60ec7affaa863b703759d52c6
Davide Caminati <davide.caminati@gmail.com>
2013-11-19 05:16:23 -0800

    translate to english

--- Hardware/Windows Switch MCP23017/lettura_sonde_apertura_finestre_con_MCP23017.py
+++ Hardware/Windows Switch MCP23017/lettura_sonde_apertura_finestre_con_MCP23017.py
@@ -1,13 +1,18 @@
 import smbus
 import time
 import redis
+#variable
+server_redis = '192.168.0.208' # IP of server redis (suggest to use the display TFT ip)
+probe_name = 'finestre' # name in redis
+DEVICE = 0x24 # Device address (A0-A2)
 
-r = redis.StrictRedis(host='192.168.0.205', port=6379, db=0)
+
+r = redis.StrictRedis(host=server_redis, port=6379, db=0)
 
 #bus = smbus.SMBus(0)  # Rev 1 Pi uses 0
 bus = smbus.SMBus(1) # Rev 2 Pi uses 1
 
-DEVICE = 0x24 # Device address (A0-A2)
+
 IODIRA = 0x00 # Pin direction register
 GPIOA  = 0x12 # Register for inputs
 
@@ -24,6 +29,6 @@ MySwitch = bus.read_byte_data(DEVICE,GPIOA)
 #if MySwitch :
    #& 0b10000000 == 0b10000000:
    #print "Switch was pressed %s" % MySwitch
-r.rpush('finestre', MySwitch)
+r.rpush(probe_name, MySwitch)
    #print r.lrange('finestre',0,-1)
    #time.sleep(1)

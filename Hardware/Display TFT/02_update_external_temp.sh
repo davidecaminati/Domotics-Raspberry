391f3f293128dec4cd833dc39b0f621eaea25f34
Davide Caminati <davide.caminati@gmail.com>
2013-11-19 08:43:38 -0800

    uodate temperature external

--- Hardware/Display TFT/02_update_external_temp.sh
+++ Hardware/Display TFT/02_update_external_temp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 esterna=`/usr/bin/elinks -dump "http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=IFCCESEN1&format=1"|awk -F "," '{print $2}'| grep '[0-9]\.[0-9]'| tail -1`
-/usr/bin/redis-cli -h 192.168.0.208 rpush temp_esterna $esterna
+/usr/bin/redis-cli -h 192.168.0.208 rpush temp_ext $esterna
+/usr/bin/redis-cli -h 192.168.0.208 rpush temp_ext_ts "`date "+%s"`"
 /usr/bin/redis-cli -h 192.168.0.208 rpush timestamp "`date "+%s"`"

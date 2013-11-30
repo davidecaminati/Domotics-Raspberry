9567d0cd21ce26d48b43f861856ff62662606bdc
Davide Caminati <davide.caminati@gmail.com>
2013-11-29 18:02:51 -0800

    improve webapi for send notification

--- Software/Send_push_notification/Send_push.py
+++ Software/Send_push_notification/Send_push.py
@@ -1,15 +1,15 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 import httplib, urllib 
 
 #variable
 hostIP = '192.168.0.208'
 app = Flask(__name__)
-
+#app.debug = True
 @app.route('/')
 def api_root():
     return 'Welcome on %s' % hostIP
 
-@app.route('/send_push/<string:pushtext>/<string:title>')
+@app.route('/send_push/',methods=['GET','POST'])
 def api_send_push(pushtext,title):
     conn = httplib.HTTPSConnection("api.pushover.net:443")
     conn.request("POST", "/1/messages.json",urllib.urlencode({"token": "abdf67yAvRcQufveo2nGkwKNi6xTHb","user": "u2v1vYFWvmGGNGN3Ffnn9NnCW1Y3xN","message": pushtext ,"title": title}), { "Content-type": "application/x-www-form-urlencoded" })
--- Software/Send_push_notification/round_robin_ping.py
+++ Software/Send_push_notification/round_robin_ping.py
@@ -2,6 +2,8 @@ import httplib, urllib
 import subprocess 
 import time
 import urllib
+import urllib2
+
 #205 digital probe
 #208 monitor TFT  server web
 #86 PC windows
@@ -14,63 +16,30 @@ while True:
     for ping in ip_device_list:
         print ip_device_list
         print ip_device_list_Error
-        
         if (ping in ip_device_list_Error) == False:
             address = "192.168.0." + str(ping)
             res = subprocess.call(['ping', '-c', '1', address])
             if res == 0:
                 print "ping to", address, "OK "
-                #conn = httplib.HTTPSConnection("api.pushover.net:443")
-                #conn.request("POST", "/1/messages.json",urllib.urlencode({"token": "abdf67yAvRcQufveo2nGkwKNi6xTHb","user": "u2v1vYFWvmGGNGN3Ffnn9NnCW1Y3xN","message": "hello world",}), { "Content-type": "application/x-www-form-urlencoded" })
-                #conn.getresponse()
-            elif res == 2:
-                print "no response from", address
-                # conn = httplib.HTTPSConnection("api.pushover.net:443")
-                # conn.request("POST", "/1/messages.json",urllib.urlencode({"token": "abdf67yAvRcQufveo2nGkwKNi6xTHb","user": "u2v1vYFWvmGGNGN3Ffnn9NnCW1Y3xN","message": "error from  %s" % address,"title": "Probe not avaible"}), { "Content-type": "application/x-www-form-urlencoded" })
-                # conn.getresponse()
-                message = "error from  %s" % str(ping)
-                title = "Probe not avaible"
-                msgToSend = urlForNotification + str(message) +  "/" + title
-                NewmsgToSend = urllib.quote_plus(msgToSend)
-                res = urllib.urlopen(msgToSend)
-                print NewmsgToSend
-                ip_device_list_Error.append(ping)
             else:
-                print "ping to", address, "failed!"
-                # conn = httplib.HTTPSConnection("api.pushover.net:443")
-                # conn.request("POST", "/1/messages.json",urllib.urlencode({"token": "abdf67yAvRcQufveo2nGkwKNi6xTHb","user": "u2v1vYFWvmGGNGN3Ffnn9NnCW1Y3xN","message": "error from  %s" % address,"title": "Probe not avaible"}), { "Content-type": "application/x-www-form-urlencoded" })
-                # conn.getresponse()
+                print "no response from", address
                 message = "error from  %s" % str(ping)
                 title = "Probe not avaible"
-                msgToSend = urlForNotification + str(message) +  "/" + title
-                NewmsgToSend = urllib.quote_plus(msgToSend)
-                res = urllib.urlopen(msgToSend)
-                print NewmsgToSend
+                data = urllib.urlencode({'pushtext': str(message),'title': title})
+                req = urllib2.Request(urlForNotification,data)
+                urllib2.urlopen(req)
                 ip_device_list_Error.append(ping)
         else:
             address = "192.168.0." + str(ping)
             res = subprocess.call(['ping', '-c', '1', address])
             if res == 0:
                 print "ping to", address, "OK "
-                # conn = httplib.HTTPSConnection("api.pushover.net:443")
-                # conn.request("POST", "/1/messages.json",urllib.urlencode({"token": "abdf67yAvRcQufveo2nGkwKNi6xTHb","user": "u2v1vYFWvmGGNGN3Ffnn9NnCW1Y3xN","message": "Probe %s is now working yet" % address,"title": "Probe Finded"}), { "Content-type": "application/x-www-form-urlencoded" })
-                # conn.getresponse()
                 message = "Probe %s is now working yet" % str(ping)
                 title = "Probe Finded"
-                msgToSend = urlForNotification + str(message) +  "/" + title
-                NewmsgToSend = urllib.quote_plus(msgToSend)
-                res = urllib.urlopen(msgToSend)
-                print NewmsgToSend
+                data = urllib.urlencode({'pushtext': str(message),'title': title})
+                req = urllib2.Request(urlForNotification,data)
+                urllib2.urlopen(req)
                 ip_device_list_Error.remove(ping)
-            elif res == 2:
-                print "no response from", address
-                #conn = httplib.HTTPSConnection("api.pushover.net:443")
-                #conn.request("POST", "/1/messages.json",urllib.urlencode({"token": "abdf67yAvRcQufveo2nGkwKNi6xTHb","user": "u2v1vYFWvmGGNGN3Ffnn9NnCW1Y3xN","message": "error from  %s" % address,"title": "Probe not avaible"}), { "Content-ty$
-                #conn.getresponse()
             else:
                 print "ping to", address, "failed!"
-                #conn = httplib.HTTPSConnection("api.pushover.net:443")
-                #conn.request("POST", "/1/messages.json",urllib.urlencode({"token": "abdf67yAvRcQufveo2nGkwKNi6xTHb","user": "u2v1vYFWvmGGNGN3Ffnn9NnCW1Y3xN","message": "error from  %s" % address,"title": "Probe not avaible"}), { "Content-ty$
-                #conn.getresponse()
-
     time.sleep(60)

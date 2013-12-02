b549ead830a75c9aca94174882ebd978269fb44d
Davide Caminati <davide.caminati@gmail.com>
2013-12-01 16:53:00 -0800

    add watch dog for process running

---
+++ Software/Check_if_process_running/check_process.py
@@ -0,0 +1,20 @@
+import re
+import subprocess
+import time
+
+#def is_running(process):
+
+while True
+#variable
+    running = False
+    
+    s = subprocess.Popen(["ps", "axw"],stdout=subprocess.PIPE)
+    for x in s.stdout:
+    
+        if re.search(process, x):
+            running = true
+            print "run"
+    if running = False
+        print "not run"
+	time.sleep(1)
+    #- See more at: http://www.bloggerpolis.com/2011/05/how-to-check-if-a-process-is-running-using-python/#sthash.6CQPQvf6.dpuf#

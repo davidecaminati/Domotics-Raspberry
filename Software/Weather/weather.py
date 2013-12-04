14567c9bb79716fa8df81e3d6426ecf77c5fc5cb
Davide Caminati <davide.caminati@gmail.com>
2013-12-04 13:51:13 -0800

    debug

--- Software/Weather/weather.py
+++ Software/Weather/weather.py
@@ -14,29 +14,28 @@ message = ''
 
 #print "Weather.com says: It is " + string.lower(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in Cesena.\n\n" + "icon is http://s.imwx.com/v.20131006.214956/img/wxicon/120/" +weather_com_result['current_conditions']['icon'] + ".png"
 
-while True:
-    #get weather
-    weather_com_result = pywapi.get_weather_from_weather_com('ITER1302')
-    temp = string.lower(weather_com_result['current_conditions']['temperature'])
-    cond = string.lower(weather_com_result['current_conditions']['text'])
-    ico = "http://s.imwx.com/v.20131006.214956/img/wxicon/120/" + weather_com_result['current_conditions']['icon'] + ".png"
-    
-    pool = redis.ConnectionPool(host=server_redis, port=6379, db=0)
-    r = redis.Redis(connection_pool=pool)
-    r.rpush("current_temp_ext",str(temp))
-    r.rpush("current_condition_ext",str(cond))
-    r.rpush("current_ico_ext",str(ico))
-    
-    
-    
-    #message
-    message = "fat fred!"
-    title = "temperatura" +  temp + " cond "  + cond 
-    
-    if notification :
-        data = urllib.urlencode({'pushtext': str(message),'title': str(title)})
-        req = urllib2.Request(urlForNotification,data)
-        response = urllib2.urlopen(req)
-        html = response.read()
-        message= '' 
-    time.sleep(600)
+#get weather
+weather_com_result = pywapi.get_weather_from_weather_com('ITER1302')
+temp = string.lower(weather_com_result['current_conditions']['temperature'])
+cond = string.lower(weather_com_result['current_conditions']['text'])
+ico = "http://s.imwx.com/v.20131006.214956/img/wxicon/120/" + weather_com_result['current_conditions']['icon'] + ".png"
+
+pool = redis.ConnectionPool(host=server_redis, port=6379, db=0)
+r = redis.Redis(connection_pool=pool)
+r.rpush("current_temp_ext",str(temp))
+r.rpush("current_condition_ext",str(cond))
+r.rpush("current_ico_ext",str(ico))
+
+
+
+#message
+message = "fat fred!"
+title = "temperatura" +  temp + " cond "  + cond 
+
+if notification :
+    data = urllib.urlencode({'pushtext': str(message),'title': str(title)})
+    req = urllib2.Request(urlForNotification,data)
+    response = urllib2.urlopen(req)
+    html = response.read()
+    message= '' 
+time.sleep(600)

9067a9a2665f7c7a5013d9777dd16667307e635d
Davide Caminati <davide.caminati@gmail.com>
2013-12-04 17:14:17 -0800

    clean code

--- Hardware/Windows Switch MCP23017/windows_doors_probe.py
+++ Hardware/Windows Switch MCP23017/windows_doors_probe.py
@@ -3,32 +3,22 @@ import time
 import redis
 #variable
 server_redis = '192.168.0.208' # IP of server redis (suggest to use the display TFT ip)
-probe_name = 'windows_doors_switch' # name in redis for this value
+redis_probe_name = 'windows_doors_switch' # name in redis for this value
 DEVICE = 0x24 # Device address (A0-A2)
 
-
 r = redis.StrictRedis(host=server_redis, port=6379, db=0)
-
-#bus = smbus.SMBus(0)  # Rev 1 Pi uses 0
 bus = smbus.SMBus(1) # Rev 2 Pi uses 1
-
-
 IODIRA = 0x00 # Pin direction register
 GPIOA  = 0x12 # Register for inputs
 
-# Set first 6 GPA pins as outputs and
-# last one as input.
+# Set pin
 bus.write_byte_data(DEVICE,IODIRA,0x80)
 
-# Loop until user presses CTRL-C
-#while True:
-
-  # Read state of GPIOA register
+# Read state of GPIOA register
 MySwitch = bus.read_byte_data(DEVICE,GPIOA)
-  
+
 #if MySwitch :
    #& 0b10000000 == 0b10000000:
-   #print "Switch was pressed %s" % MySwitch
-r.rpush(probe_name, MySwitch)
-   #print r.lrange('finestre',0,-1)
-   #time.sleep(1)
+
+#write to redis
+r.rpush(redis_probe_name, MySwitch)

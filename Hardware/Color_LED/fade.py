28ec2ead5aeb8b5408239e4a1687ec46ab5af4d0
Davide Caminati <davide.caminati@gmail.com>
2013-12-11 19:07:49 -0800

    added xbee.py

--- Hardware/Color_LED/fade.py
+++ Hardware/Color_LED/fade.py
@@ -1,10 +1,12 @@
 import RPIO.PWM as PWM
 import time
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 
 #variable
 hostIP = '192.168.0.100'
 app = Flask(__name__)
+app.config.from_object(__name__)
+
 GPIO_RED = 17
 GPIO_GREEN = 27
 GPIO_BLUE = 22
@@ -22,14 +24,28 @@ def api_root():
     return 'Welcome on %s' % hostIP
 
 #PWM.set_loglevel(PWM.LOG_LEVEL_DEBUG)
-@app.route('/colorchange/<int:r>/<int:g>/<int:b>', methods=['POST','GET'])
-def api_colorchange(r,g,b):
+@app.route('/colorchange', methods=['GET', 'POST'])
+def api_colorchange():
 #PWM.print_channel(CHANNEL)
-    PWM.add_channel_pulse(CHANNEL, GPIO_RED, 0, r)
-    PWM.add_channel_pulse(CHANNEL, GPIO_GREEN, 0, g)
-    PWM.add_channel_pulse(CHANNEL, GPIO_BLUE, 0, b)
+    if request.method == 'POST':
+        red = request.form['r']
+        green = request.form['g']
+        blue = request.form['b']
+ 
+        PWM.add_channel_pulse(CHANNEL, GPIO_RED, 0, int(red))
+        PWM.add_channel_pulse(CHANNEL, GPIO_GREEN, 0, int(green))
+        PWM.add_channel_pulse(CHANNEL, GPIO_BLUE, 0, int(blue))
+        return 'post %s' % r
+    else:
+        return 'get'
+    #red = session.get('r')
+    #green = session.get('g')
+    #blue = session.get('b')
+    #PWM.add_channel_pulse(CHANNEL, GPIO_RED, 0, int(red))
+    #PWM.add_channel_pulse(CHANNEL, GPIO_GREEN, 0, int(green))
+    #PWM.add_channel_pulse(CHANNEL, GPIO_BLUE, 0, int(blue))
     #time.sleep(0.01)
-    return 'r:%s g:%s b:%s' % (r,g,b)
+    #return x
 #PWM.add_channel_pulse(CHANNEL, GPIO_BLUE, 100, 50)
 
 #time.sleep(5)
@@ -37,5 +53,15 @@ def api_colorchange(r,g,b):
 #PWM.clear_channel_gpio(0, GPIO_BLUE)
 
 #PWM.cleanup()
+@app.route('/colorchange2/<string:red>/<string:green>/<string:blue>', methods=['GET', 'POST'])
+def api_colorchange2(red,green,blue):
+#PWM.print_channel(CHANNEL)
+    
+    PWM.add_channel_pulse(CHANNEL, GPIO_RED, 0, int(red))
+    PWM.add_channel_pulse(CHANNEL, GPIO_GREEN, 0, int(green))
+    PWM.add_channel_pulse(CHANNEL, GPIO_BLUE, 0, int(blue))
+    return 'ok'
+        
 if __name__ == '__main__':
+    app.debug=True
     app.run(host=hostIP)

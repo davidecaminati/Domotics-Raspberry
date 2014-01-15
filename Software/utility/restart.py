065cab8f593f18fbb37e80480243107cf45fbad6
Davide Caminati <davide.caminati@gmail.com>
2014-01-15 05:33:43 -0800

    added restart and shutdown buttons

---
+++ Software/utility/restart.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import subprocess
+import RPi.GPIO as GPIO
+
+channel=24
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.wait_for_edge(channel, GPIO.FALLING)
+
+subprocess.call(['/sbin/reboot'])

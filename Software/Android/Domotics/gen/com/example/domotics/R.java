3565c9a6a92eeac6838468c84295b06a681a7e02
Davide Caminati <davide.caminati@gmail.com>
2014-02-21 21:10:11 -0800

    added door open button

--- Software/Android/Domotics/gen/com/example/domotics/R.java
+++ Software/Android/Domotics/gen/com/example/domotics/R.java
@@ -24,13 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080008;
         public static final int button1=0x7f080000;
         public static final int button2=0x7f080001;
         public static final int button3=0x7f080002;
         public static final int button4=0x7f080003;
         public static final int button5=0x7f080004;
         public static final int button6=0x7f080005;
+        public static final int button7=0x7f080006;
+        public static final int editText1=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- Software/Android/Domotics/src/com/example/domotics/MainActivity.java
+++ Software/Android/Domotics/src/com/example/domotics/MainActivity.java
@@ -1,17 +1,37 @@
 package com.example.domotics;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+//import android.os.RemoteException;
 import android.widget.Button;
+//import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
+import java.io.InputStream;
 
-public class MainActivity extends Activity {
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+//import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+//import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
 
-	Button buttonSalaON,buttonCucinaON,buttonIngressoON,buttonSalaOFF,buttonCucinaOFF,buttonIngressoOFF;
+//import com.authorwjf.http_get.R;
+//import com.authorwjf.http_get.Main.LongRunningGetIO;
+
+import java.io.IOException;
+
+
+public class MainActivity extends Activity  {
+
+	Button buttonPorta,buttonSalaON,buttonCucinaON,buttonIngressoON,buttonSalaOFF,buttonCucinaOFF,buttonIngressoOFF;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +40,11 @@ public class MainActivity extends Activity {
 		
 		addListenerOnButton();
 
-	}
+		}
+	
 		public void addListenerOnButton() {
+
+			buttonPorta = (Button) findViewById(R.id.button7);
 
 			buttonSalaON = (Button) findViewById(R.id.button1);
 			buttonCucinaON = (Button) findViewById(R.id.button2);
@@ -32,52 +55,125 @@ public class MainActivity extends Activity {
 			buttonIngressoOFF = (Button) findViewById(R.id.button6);
 			
 
+			buttonPorta.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				  	//Intent browserIntent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.202:5000/reletimer/8/500"));
+				    //startActivity(browserIntent);
+				new LongRunningGetIO().execute("http://192.168.0.202:5000/reletimer/8/500");
+				}
+			});	 
+			
 			buttonSalaON.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View arg0) {
-					  	Intent browserIntent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.202:5000/releon/5"));
-					    startActivity(browserIntent);
-					}
-				});	 
+			@Override
+			public void onClick(View arg0) {
+				  	//Intent browserIntent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.202:5000/releon/5"));
+				    //startActivity(browserIntent);
+				new LongRunningGetIO().execute("http://192.168.0.202:5000/releon/5");
+				}
+			});	 
 			
 			buttonCucinaON.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View arg0) {
-						Intent browserIntent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.202:5000/releon/4"));
-						startActivity(browserIntent);
-					}
-				});	 
+			@Override
+			public void onClick(View arg0) {
+					//Intent browserIntent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.202:5000/releon/4"));
+					//startActivity(browserIntent);
+				new LongRunningGetIO().execute("http://192.168.0.202:5000/releon/4");
+				}
+			});	 
 			
 			buttonIngressoON.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View arg0) {
-						Intent browserIntent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.202:5000/releon/3"));
-						startActivity(browserIntent);
-					}
-				});
+			@Override
+			public void onClick(View arg0) {
+					//Intent browserIntent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.202:5000/releon/3"));
+					//startActivity(browserIntent);
+				new LongRunningGetIO().execute("http://192.168.0.202:5000/releon/3");
+				}
+			});
+			
 			buttonSalaOFF.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View arg0) {
-					  	Intent browserIntent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.202:5000/releoff/5"));
-					    startActivity(browserIntent);
-					}
-				});	 
+			@Override
+			public void onClick(View arg0) {
+				  	//Intent browserIntent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.202:5000/releoff/5"));
+				    //startActivity(browserIntent);
+				new LongRunningGetIO().execute("http://192.168.0.202:5000/releoff/5");
+				}
+			});	 
 			
 			buttonCucinaOFF.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View arg0) {
-						Intent browserIntent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.202:5000/releoff/4"));
-						startActivity(browserIntent);
-					}
-				});	 
+			@Override
+			public void onClick(View arg0) {
+					//Intent browserIntent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.202:5000/releoff/4"));
+					//startActivity(browserIntent);
+				new LongRunningGetIO().execute("http://192.168.0.202:5000/releoff/4");
+				}
+			});	 
 			
 			buttonIngressoOFF.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View arg0) {
-						Intent browserIntent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.202:5000/releoff/3"));
-						startActivity(browserIntent);
-					}
-				});
-		}
+			@Override
+			public void onClick(View arg0) {
+					//Intent browserIntent =  new Intent(Intent.ACTION_VIEW, Uri.parse("http://192.168.0.202:5000/releoff/3"));
+					//startActivity(browserIntent);
 
+				//EditText et = (EditText)findViewById(R.id.editText1);
+				//new LongRunningGetIO().execute();
+				
+				
+				//Button b = (Button)findViewById(R.id.button6);
+				//b.setClickable(false);
+				new LongRunningGetIO().execute("http://192.168.0.202:5000/releoff/3");
+
+				}
+			});
+		}
+		
+		private class LongRunningGetIO extends AsyncTask <String, Integer, String> {
+			
+			protected String getASCIIContentFromEntity(HttpEntity entity) throws IllegalStateException, IOException {
+
+			InputStream in = entity.getContent();
+			StringBuffer out = new StringBuffer();
+			int n = 1;
+			while (n>0) 
+			{
+				byte[] b = new byte[4096];
+		
+				n =  in.read(b);
+		
+				if (n>0) out.append(new String(b, 0, n));
+			}
+			return out.toString();
+	
+		}							
+
+
+		@Override
+		protected String doInBackground(String... urls) {
+			String text = null;
+
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpContext localContext = new BasicHttpContext();
+			HttpGet httpGet = new HttpGet(urls[0]);
+			
+			try 
+			{
+				HttpResponse response = httpClient.execute(httpGet, localContext);
+			    HttpEntity entity = response.getEntity();
+			    text = getASCIIContentFromEntity(entity);
+			} 
+			catch (Exception e) 
+			{
+				return e.getMessage();
+			}
+			return text;
+		}
+		
+		protected void onPostExecute(String results) {
+			if (results!=null) {
+				EditText et = (EditText)findViewById(R.id.editText1);
+				et.setText(results);
+			}
+		}
+	}
 }
+

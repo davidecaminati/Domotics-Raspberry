5744870a3bded1dfa9ab1c22583ac26b9977f4bc
Davide Caminati <davide.caminati@gmail.com>
2014-02-24 16:06:42 -0800

    button door added

--- Hardware/Wall switch/wall_Switch.py
+++ Hardware/Wall switch/wall_Switch.py
@@ -5,7 +5,6 @@ import urllib2
 import looper
 import threading
 
-
     
 # IODIRA   0x00   // IO direction  (0 = output, 1 = input (Default))
 # IODIRB   0x01
@@ -58,6 +57,10 @@ urlForTimer = 'http://192.168.0.202:5000/reletimer/'
 urlForDimmOn = 'http://192.168.0.202:5000/reledimmon/'
 #Url for dimm off
 urlForDimmOff = 'http://192.168.0.202:5000/reledimmoff/'
+#urlForMultiOff
+urlForMultiOff = 'http://192.168.0.202:5000/multireleoff/' # need 5 number
+#urlForMultiOn
+urlForMultiOn = 'http://192.168.0.202:5000/multireleon/' # need 5 number
 
 # Set output all 7 output bits to 0
 bus.write_byte_data(DEVICE,OLATA,0xC0) #11000000 A
@@ -106,40 +109,28 @@ class ButtonsState():
     StillPressed = 2
     LongPressed = 3
     
-    
+
 class Home(object):
+    
     def __init__(self, name, roomlist = []):
         self.name = name
         self.roomlist = roomlist
-    
+                
     def __str__(self):
         return self.name
 
     def __repr__(self):
         return "<Home: %s>" % self
    
-    def LightsOff(self):
-        for r in self.roomlist:
-            for l in r.lamplist:
-                l.off()
-                
-    def LightsOn(self):
-        for r in self.roomlist:
-            for l in r.lamplist:
-                l.on()
-                
-    def LightsToggle(self):
-        for r in self.roomlist:
-            for l in r.lamplist:
-                l.toggle()
    
 class Room(object):
-    def __init__(self, name, lamplist = [], buttonlist = [],sensorlist = [],actuatorlist = []):
+    def __init__(self, name, lamplist = [], buttonlist = [],sensorlist = [],actuatorlist = [], doorlist = []):
         self.name = name
         self.lamplist = lamplist
         self.buttonlist = buttonlist
         self.sensorlist = sensorlist
         self.actuatorlist = actuatorlist
+        self.doorlist = doorlist
         
     def __str__(self):
         return self.name
@@ -159,6 +150,25 @@ class Room(object):
         for l in self.lamplist:
             l.toggle()
         
+        
+class Door(object):
+    def __init__(self, name, position, releOn):
+        self.name = name
+        self.position = position
+        self.releOn = releOn
+        
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return "<lamp: %s>" % self 
+     
+    def open(self):
+        response = urllib2.urlopen(urlForTimer + str(self.releOn) + "/500")
+        html = response.read()
+        return "ok"
+
+     
 class Lamp(object):
     def __init__(self, name, type, position, releOn, isdimmable, releDimm):
         self.name = name
@@ -220,9 +230,87 @@ class Lamp(object):
             return "ok"
         else:
             return "not dimmable"
- 
-class Button(object):
+    
+class MainController(object):    
+    
+    def __init__(self, name,home,urlForToggle,urlForMultiOff,urlForMultiOn):
+        self.name = name
+        self.home = home
+        self.urlForToggle = urlForToggle
+        self.urlForMultiOff = urlForMultiOff
+        self.urlForMultiOn = urlForMultiOn
+        self.lampList = []
+        self.__popolateLampList__()
+        
+                  
+    def group_iter(self,iterator, n=1):
+        """ Given an iterator, it returns sub-lists made of n items
+        (except the last that can have len < n)
+        inspired by http://countergram.com/python-group-iterator-list-function"""
+        accumulator = []
+        for item in iterator:
+            accumulator.append(item)
+            if len(accumulator) == n: # tested as fast as separate counter
+                yield accumulator
+                accumulator = [] # tested faster than accumulator[:] = []
+                # and tested as fast as re-using one list object
+        if len(accumulator) != 0:
+            yield accumulator
 
+    def __popolateLampList__(self):
+        for r in self.home.roomlist:
+            for l in r.lamplist:
+                self.lampList.append(l.releOn)
+        
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return "<Button: %s>" % self
+    
+    def LightsOff(self):
+        if len(self.lampList) > 5:
+            #need split
+            groups = self.group_iter(self.lampList,5)
+            for g in groups:
+                # i need to be sure than i can take 5 element 
+                raw = g*5
+                response = urllib2.urlopen(urlForMultiOff + str(raw[0]) + "/"  + str(raw[1]) + "/" + str(raw[2]) + "/" + str(raw[3]) + "/" + str(raw[4]))
+                html = response.read()
+                return "ok"
+        else:
+            #send list to webservice
+            if len(self.lampList) != 0:
+                raw = self.lampList * 5
+                response = urllib2.urlopen(urlForMultiOff + str(raw[0]) + "/"  + str(raw[1]) + "/" + str(raw[2]) + "/" + str(raw[3]) + "/" + str(raw[4]))
+                html = response.read()
+                return "ok"
+            else:
+                return "allready done"
+            
+    def LightsOn(self):
+        if len(self.lampList) > 5:
+            #need split
+            groups = self.group_iter(self.lampList,5)
+            for g in groups:
+                # i need to be sure than i can take 5 element 
+                raw = g*5
+                response = urllib2.urlopen(urlForMultiOn + str(raw[0]) + "/"  + str(raw[1]) + "/" + str(raw[2]) + "/" + str(raw[3]) + "/" + str(raw[4]))
+                html = response.read()
+                return "ok"
+        else:
+            #send list to webservice
+            if len(self.lampList) != 0:
+                raw = self.lampList * 5
+                response = urllib2.urlopen(urlForMultiOn + str(raw[0]) + "/"  + str(raw[1]) + "/" + str(raw[2]) + "/" + str(raw[3]) + "/" + str(raw[4]))
+                html = response.read()
+                return "ok"
+            else:
+                return "allready done"
+
+
+
+class Button(object):
     def __init__(self, name, inputpin,BinLedNumberRED,BinLedNumberGREEN,model,controlled):
         self.name = name
         self.inputpin = inputpin
@@ -389,6 +477,9 @@ class Button(object):
             NewBinLedNumber =  OldStateA & ( ~ (self.BinLedNumberRED |  self.BinLedNumberGREEN))
         bus.write_byte_data(DEVICE,GPIOA,NewBinLedNumber)
             
+# ---- door -----
+Entrance_door = Door("Entrance Door", "Entrance", 8)
+
 # ---- lamp -----
 Lamp_holder_lounge_dimm = Lamp("lamp holder dimm",LampType.Led,"lounge",1,True,6)
 Lamp_holder_entrance = Lamp("lamp holder",LampType.Led,"Entrance",3,False,0)
@@ -403,9 +494,11 @@ Lamp_holder_lounge = Lamp("lamp holder",LampType.Led,"lounge",5,False,0)
 # Create Room
 Launge = Room("Launge",[Lamp_holder_lounge_dimm,Lamp_holder_lounge])
 Kitchen = Room("Kitchen",[Lamp_holder_kitchen])
-Entrance = Room("Entrance",[Lamp_holder_entrance])
+Entrance = Room("Entrance",[Lamp_holder_entrance],None,None,None,Entrance_door)
 #Create Home
 MyHome = Home("Home",[Launge,Kitchen,Entrance])
+MyController = MainController("Main Controller",MyHome,urlForToggle,urlForMultiOff,urlForMultiOn)
+
 
 ButtonLaunge_SX = Button("ButtonLaunge_SX",0,0b00000100,0b00001000,model,Launge)
 ButtonLaunge_DX = Button("ButtonLaunge_DX",0,0b00010000,0b00100000,model,Entrance)
@@ -413,6 +506,7 @@ ButtonKitchen = Button("ButtonKitchen",0,0b00000001,0b00000010,model,Kitchen)
 ButtonEntrance_SX = Button("ButtonEntrance_SX",0,0b01000000,0b10000000,model,MyHome)
 ButtonEntrance_DX = Button("ButtonEntrance_DX",0,0b01000000,0b10000000,model,Entrance) #A
 
+model.daemon = True # non blocking thread
 model.start()
 
 
@@ -446,6 +540,9 @@ def CheckColorForLeds():
     else:
         ButtonEntrance_SX.SetLedColorA(LedColor.Red)
 
-CheckColorForLeds()
+
+while True:
+    CheckColorForLeds()
+    time.sleep(5)
 
         

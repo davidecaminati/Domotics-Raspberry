5a3c9abd42bc64e90610820744bd8454e2eaf6b4
Davide Caminati <davidecaminati@users.noreply.github.com>
2014-06-12 01:43:47 +0200

    updated RadioStreaming + Redis API

--- Software/RadioStreaming/radioStreaming.py
+++ Software/RadioStreaming/radioStreaming.py
@@ -2,8 +2,13 @@ import urllib
 import urllib2
 import pygame
 import subprocess
+import redis
+import time
 from random import randint
 from flask import Flask, url_for #, render_template, request, jsonify
+
+
+server_redis = '127.0.0.1'
 
 app = Flask(__name__)
 
@@ -32,8 +37,30 @@ def api_play(number):
 @app.route('/stop')
 def api_stop():
     k = subprocess.Popen(['killall', 'mpg123'])
-    return "stop"
+    return "stop" , 201, {'Access-Control-Allow-Origin': '*'} 
+    
+    
+@app.route('/setredis/<campo>/<valore>')
+def api_setredis(campo,valore):
+    pool = redis.ConnectionPool(host=server_redis, port=6379, db=0)
+    r = redis.Redis(connection_pool=pool)
+    ts = time.time()
+    r.rpush(campo,str(valore))
+    r.rpush(campo + "timestamp",str(ts))
+    return "ok", 201, {'Access-Control-Allow-Origin': '*'} 
+    
+@app.route('/getredis/<campo>')
+def api_getredis(campo):
+    pool = redis.ConnectionPool(host=server_redis, port=6379, db=0)
+    r = redis.Redis(connection_pool=pool)
+    valori = r.lrange(campo,'-1','-1')
+    valore = ""
+    if len(valori) >= 1:
+        valore = valori[0]
+    else:
+        valore = "errore"
+    return valore , 201, {'Access-Control-Allow-Origin': '*'} 
     
     
 if __name__ == '__main__':
-    app.run(host='192.168.0.110',debug = True)
+    app.run(host='192.168.0.208',debug = True)

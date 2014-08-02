5dd1c206f3b232ad0dc6a251440a82c9a03e9e57
Davide Caminati <davidecaminati@users.noreply.github.com>
2014-08-02 02:19:34 +0200

    Arduino I2C scanner + Ultrasonic + WebServer + how to fix Bricked NANO

---
+++ Hardware/Arduino Ultrasonic/Ultrasonic/Ultrasonic.ino
@@ -0,0 +1,13 @@
+#include <Ultrasonic.h>
+
+
+Ultrasonic ultrasonic(12,13);
+void setup() {
+Serial.begin(9600);
+}
+void loop()
+{
+  Serial.print(ultrasonic.Ranging(CM));
+  Serial.println("cm");    
+  delay(100);
+}

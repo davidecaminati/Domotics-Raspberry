fb775a84eaa4aa45a8c1ca5740cc24d15ec913c7
Davide Caminati <davidecaminati@users.noreply.github.com>
2016-06-12 03:04:13 +0200

    added i2c test

---
+++ Test_i2c/Arduino_i2c_slave_ID5/Arduino_i2c_slave_ID5.ino
@@ -0,0 +1,71 @@
+/* Source code downloaded from
+ *  https://blog.retep.org/2014/02/15/connecting-an-arduino-to-a-raspberry-pi-using-i2c/
+ * 
+ *  Thanks Peter Mount
+ */
+
+#include <Wire.h>
+ 
+#define SLAVE_ADDRESS 0x05
+int number = 0;
+int state = 0;
+ 
+double temp;
+ 
+void setup() {
+ pinMode(13, OUTPUT);
+ 
+ // initialize i2c as slave
+ Wire.begin(SLAVE_ADDRESS);
+ 
+ // define callbacks for i2c communication
+ Wire.onReceive(receiveData);
+ Wire.onRequest(sendData);
+}
+ 
+void loop() {
+ delay(100);
+ temp = GetTemp();
+}
+ 
+// callback for received data
+void receiveData(int byteCount){
+ 
+ while(Wire.available()) {
+  number = Wire.read();
+ 
+  if (number == 1){
+   if (state == 0){
+    digitalWrite(13, HIGH); // set the LED on
+    state = 1;
+   } else{
+    digitalWrite(13, LOW); // set the LED off
+    state = 0;
+   }
+  }
+ 
+  if(number==2) {
+   number = (int)temp;
+  }
+ }
+}
+ 
+// callback for sending data
+void sendData(){
+ Wire.write(number);
+}
+ 
+// Get the internal temperature of the arduino
+double GetTemp(void)
+{
+ unsigned int wADC;
+ double t;
+ ADMUX = (_BV(REFS1) | _BV(REFS0) | _BV(MUX3));
+ ADCSRA |= _BV(ADEN); // enable the ADC
+ delay(20); // wait for voltages to become stable.
+ ADCSRA |= _BV(ADSC); // Start the ADC
+ while (bit_is_set(ADCSRA,ADSC));
+ wADC = ADCW;
+ t = (wADC - 324.31 ) / 1.22;
+ return (t);
+}
---
+++ Test_i2c/README.txt
@@ -0,0 +1,3 @@
+Find more information 
+
+https://blog.retep.org/2014/02/15/connecting-an-arduino-to-a-raspberry-pi-using-i2c/
